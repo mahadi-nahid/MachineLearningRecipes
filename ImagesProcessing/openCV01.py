100aa7628cb630a39723df127574a89b9058ce92
Md. Mahadi Hasan Nahid <nahid@student.sust.edu>
2017-03-07 18:26:07 +0600

    image read and convert np array

--- ImagesProcessing/openCV01.py
+++ ImagesProcessing/openCV01.py
@@ -10,7 +10,8 @@ import cv2
 
 #Displaying an Image using opencv
 
-img = cv2.imread('nahid.jpg',0)
+#img = cv2.imread('nahid.jpg',0)
+img = cv2.imread('mdb001.pgm', 0)
 cv2.imshow('image',img)
 k = cv2.waitKey(0)
 if k == 27:         # wait for ESC key to exit
@@ -26,7 +27,13 @@ elif k == ord('s'): # wait for 's' key to save and exit
 
 from matplotlib import pyplot as plt
 
-img = cv2.imread('nahid.jpg',0)
+img = cv2.imread('mdb001.pgm',0)
 plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
 plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
 plt.show()  
+
+print img.shape
+
+imgarr = np.array(img) 
+
+print imgarr
